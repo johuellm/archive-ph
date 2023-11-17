e1b4d8f27f1b43b9d86611f24a0cbf3e913ec44a
Joschka Hüllmann <joschka.huellmann@uni-muenster.de>
2023-11-17 10:16:59 +0100

    added contextmenu for archive.ph

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Archive.ph Link Opener
+
+Very basic web extension for Firefox that adds a context menu to quickly open links in archive.ph.
---
+++ background.js
@@ -0,0 +1,39 @@
+// Callback reads runtime.lastError to prevent an unchecked error from being 
+// logged when the extension attempt to register the already-registered menu 
+// again. Menu registrations in event pages persist across extension restarts.
+browser.contextMenus.create({
+        id: "archive-ph-click",
+        title: "Open link in &archive.ph",
+        contexts: ["link"],
+        icons: {
+          "16": "icons/16.png",
+          "32": "icons/32.png",
+          "64": "icons/64.png"
+        }
+    },
+    // See https://extensionworkshop.com/documentation/develop/manifest-v3-migration-guide/#event-pages-and-backward-compatibility
+    // for information on the purpose of this error capture.
+    () => void browser.runtime.lastError,
+);
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "archive-ph-click") {
+        // Examples: text and HTML to be copied.
+        const text = "This is text: " + info.linkUrl;
+        // Always HTML-escape external input to avoid XSS.
+        const safeUrl = escapeHTML(info.linkUrl);
+
+        browser.tabs.create({url: "https://archive.ph/oldest/"+safeUrl});
+    }
+});
+
+// https://gist.github.com/Rob--W/ec23b9d6db9e56b7e4563f1544e0d546
+function escapeHTML(str) {
+    // Note: string cast using String; may throw if `str` is non-serializable, e.g. a Symbol.
+    // Most often this is not the case though.
+    return String(str)
+        .replace(/&/g, "&amp;")
+        .replace(/"/g, "&quot;").replace(/'/g, "&#39;")
+        .replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
+
